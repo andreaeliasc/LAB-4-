ba8999e4ce3628e345c4b5a5180fd7b6fb47f6ae
DESKTOP-DMEFEJ6\Andrea <andrea.elias16@gmail.com>
2019-04-22 21:56:59 -0600

    no message

---
+++ ANDREA_ELIAS_LAB4/Assets/Disparo.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Disparo : MonoBehaviour
+{
+    private int ctr = 0;
+
+
+    public Text score;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Disparar();
+        }
+    }
+
+    void Disparar()
+    {
+
+        GetComponent<Camera>().ScreenPointToRay(Input.mousePosition);
+        Ray myRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(myRay, out hitInfo, Mathf.Infinity) && hitInfo.collider.gameObject.tag == "Blanco")
+        {
+            ctr++;
+          
+            score.text = "" + ctr;
+            Destroy(hitInfo.collider.gameObject);
+        }
+    }
+}
---
+++ ANDREA_ELIAS_LAB4/Assets/Scripts/MoverTarget.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverTarget : MonoBehaviour
+{
+
+    public float delta = 1.5f;  
+    public float speed = 2.0f;
+    private Vector3 Inicio;
+
+    void Start()
+    {
+        Inicio = transform.position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 v = Inicio;
+        v.x += delta * Mathf.Sin(Time.time * speed);
+        transform.position = v;
+    }
+}
